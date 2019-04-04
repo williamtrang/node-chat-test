f077cbaf52269b0f228757f05cb6ca99ae2d70a5
William Trang <shadowskarmory17@gmail.com>
2019-04-03 17:53:02 -0700

    working chat but not good styling

--- server.js
+++ server.js
@@ -18,7 +18,26 @@ io.sockets.on('connection',function(socket){
 
     //disconnections
     socket.on('disconnect',function(data){
+        users.splice(users.indexOf(socket.username),1);
+        updateUsernames();
         connections.splice(connections.indexOf(socket),1);
         console.log("Disconnected %s sockets connected", connections.length );
     });
+
+    socket.on('send message',function(data){
+        console.log(data);
+        io.sockets.emit('new message',{msg:data,user: socket.username});
+    });
+
+    socket.on('new user',function(data,callback){
+        callback(true);
+        socket.username = data;
+        users.push(socket.username);
+        updateUsernames();
+
+    });
+
+    function updateUsernames(){
+        io.sockets.emit('get users', users);
+    }
 });
